2148950df7784f2984af64d4d7b187cedcae82ae
George Hyros <65777363+phantomsoul1@users.noreply.github.com>
2020-09-15 16:59:03 -0400

    L1: Basic Table Content Loaded

--- ufo-level-1/static/js/app.js
+++ ufo-level-1/static/js/app.js
@@ -2,3 +2,14 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+tbody = d3.select("#ufo-table").select("tbody");
+console.log(tbody);
+
+tableData.forEach((sighting) => {
+    var row = tbody.append("tr");
+    Object.entries(sighting).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
