a4d49d06ef595ad2c9a6d5f1e5b605219f8e1a1a
George Hyros <65777363+phantomsoul1@users.noreply.github.com>
2020-09-15 17:21:09 -0400

    L1: Basic Filtration Code
    Doesn't quite filter yet...

--- ufo-level-1/static/js/app.js
+++ ufo-level-1/static/js/app.js
@@ -2,14 +2,44 @@
 var tableData = data;
 
 // YOUR CODE HERE!
-tbody = d3.select("#ufo-table").select("tbody");
-console.log(tbody);
 
-tableData.forEach((sighting) => {
-    var row = tbody.append("tr");
-    Object.entries(sighting).forEach(([key, value]) => {
-        var cell = row.append("td");
-        cell.text(value);
+
+function display(displayData) {
+    tbody = d3.select("#ufo-table").select("tbody");
+    
+    console.log(tbody);
+
+    tbody.html("");
+    displayData.forEach((sighting) => {
+        var row = tbody.append("tr");
+        Object.entries(sighting).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
     });
-});
+}
+
+d3.select("#filter-btn")
+    .on("click", () => {
+        // Prevent the page from refreshing
+        d3.event.preventDefault();
+        
+        // todo add filtering code here
+        dateinput = d3.select("#datetime");
+
+        // Get the value property of the input element
+        var datevalue = dateinput.property("value");
+
+        console.log(datevalue);
+
+        var tableData = data;
+        var filteredData = tableData.filter((sighting) => {
+            console.log(sighting.date);            
+            return sighting.date == datevalue;
+        });
+
+        display(filteredData);
+    });
+
+display(tableData);
 
